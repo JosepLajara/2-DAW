6953cb7949a77003d043456ba38e22c1495ad737
Josep <joseplajara7@gmail.com>
2018-11-29 10:12:18 +0100

    Subir clases 29/11

--- Programacion/Cliente/Clase/Hostname/script.js
+++ Programacion/Cliente/Clase/Hostname/script.js
@@ -1,3 +1,4 @@
+/* Actividad 1*/
 let enlaces = document.querySelectorAll("a");
 
 let map = new Map();
@@ -8,3 +9,21 @@ for(let enlace of enlaces){
    }
 }
 console.log(map);
+
+/*Actividad 2*/
+
+let query = document.querySelectorAll('a,.step img');
+
+console.log(query);
+
+/*Actividad 3*/
+
+let query1 = document.querySelectorAll('.step-icon ~ img');
+
+console.log(query1);
+
+/*Actividad 4*/
+
+let query2 = document.querySelectorAll("a[href='https://go.microsoft.com/fwlink/?linkid=862126']");
+
+console.log(query2);
